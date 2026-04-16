f94d0a52d103f0e2f602d55b20e1107e44ae7de8
Ezequiel Trigo <ezequiel0trigo@gmail.com>
2026-04-16 13:10:35 -0300

    Minor change

--- GetEmailsFromCsv.py
+++ GetEmailsFromCsv.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def emailsFromCsv(fileName):
     df = pd.read_csv(fileName, sep=",")
+    print("quantity: "+ str(len(df["Email"])))
     return ",".join(df["Email"])
 
 if __name__ == "__main__":
